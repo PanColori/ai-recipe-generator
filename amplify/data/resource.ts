4f356062ddc0dbc701e8c81c6a2d1fd32bcc0447
Panfilo <panfilo@iinet.net.au>
2024-08-02 22:52:58 +0800

    Add Bedrock resource

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,17 +1,31 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 /*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any unauthenticated user can "create", "read", "update", 
-and "delete" any "Todo" records.
+The section below creates a "BedrockResponse" schema of custome type
+with "body" and "error" fields and an "askBedrock" schema with a query
+field. The authorization rule below specifies that the user needs to have
+and API key
+
+The following code defines the askBedrock query that takes an array of 
+strings called ingredients and returns a BedrockResponse. 
+The .handler(a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrockDS" }))
+line sets up a custom handler for this query, defined in bedrock.js,
+using bedrockDS as its data source.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
+  BedrockResponse: a.customType({
+    body: a.string(),
+    error: a.string(),
+  }),
+
+  askBedrock: a
+    .query()
+    .arguments({ ingredients: a.string().array() })
+    .returns(a.ref("BedrockResponse"))
+    .authorization((allow) => [allow.authenticated()])
+    .handler(
+      a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrockDS" })
+    ),
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,9 +33,13 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
+
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
